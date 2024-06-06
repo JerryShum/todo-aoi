e4383ec14b2bf8b0dfd91e1a390cbb613edc2205
Jerry <jerryshum2636@gmail.com>
2024-06-06 19:52:53 -0400

    feat: define list component

---
+++ src/components/List.jsx
@@ -0,0 +1,9 @@
+import Task from "./Task";
+
+export default function List() {
+  return (
+    <div className="bg-accent-900 rounded-xl flex flex-col gap-5 p-10 w-full ">
+      <Task />
+    </div>
+  );
+}
