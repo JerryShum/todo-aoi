a4d7bc7b88ae564927815a6b361518e7c424d25b
JerryUbuntu <jerryshum2636@gmail.com>
2024-06-07 19:04:53 -0400

    feat: made emoji button bigger

--- src/components/IconButton.jsx
+++ src/components/IconButton.jsx
@@ -1,6 +1,6 @@
 export default function IconButton({ children }) {
   return (
-    <button className="bg-accent-600 rounded-xl p-2  hover:bg-accent-500 duration-300">
+    <button className="bg-accent-600 rounded-xl p-2  hover:bg-accent-500 duration-300 text-2xl">
       {children}
     </button>
   );
