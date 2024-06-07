ea536b90a8fd269d3776e2f3ba62491b4577ddff
Jerry <jerryshum2636@gmail.com>
2024-06-06 20:08:07 -0400

    feat: outline of app

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+import List from "./components/List";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="flex flex-col h-screen items-center justify-center gap-10">
+      <h1 className=" font-bold text-5xl">TODO LIST</h1>
+      <div className="w-1/2">
+        <List />
+      </div>
+    </div>
+  );
 }
