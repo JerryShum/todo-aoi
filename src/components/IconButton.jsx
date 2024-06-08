1d536c552698247415d2c82e4b41abdf60c099da
Jerry <jerryshum2636@gmail.com>
2024-06-07 21:44:40 -0400

    feat: replaced emoji with X

--- src/components/IconButton.jsx
+++ src/components/IconButton.jsx
@@ -1,6 +1,6 @@
 export default function IconButton({ children }) {
   return (
-    <button className="bg-primary-500 rounded-xl p-2  hover:bg-primary-400 duration-300 text-2xl">
+    <button className="bg-primary-500 hover:bg-primary-400 text-text-950 duration-300 h-7 w-7 rounded-lg ">
       {children}
     </button>
   );
