e2578e3c621547cccdb7cec8b98e7bb4494ca371
Jerry <jerryshum2636@gmail.com>
2024-06-07 21:47:11 -0400

    feat: conditional rendering of add task

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import Button from "./components/Button";
+import Button from "./components/AddTaskButton";
 import List from "./components/List";
 import Sort from "./components/Sort";
+import { useState } from "react";
 
 export default function App() {
+  const [addTaskView, setAddTaskView] = useState(false);
+  function handleAddTask() {
+    setAddTaskView(() => !addTaskView);
+  }
+
   return (
     <div className="flex flex-col h-screen items-center justify-center gap-10">
       <h1 className=" font-bold text-5xl">TODO LIST</h1>
       <div className="w-1/2 flex flex-col gap-5">
         <div className="flex justify-between">
-          <Button>Add Task</Button>
+          <Button addTaskView={addTaskView} handleAddTask={handleAddTask}>
+            Add Task
+          </Button>
           <Sort />
         </div>
+        {addTaskView && (
+          <input
+            type="text"
+            className="p-3 rounded-lg bg-accent-900 font-semibold"
+          />
+        )}
         <List />
       </div>
     </div>
