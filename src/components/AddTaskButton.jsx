cb913a75ce887cf05ea1c8f876dc48ddb0224eab
JerryUbuntu <jerryshum2636@gmail.com>
2024-06-08 17:12:46 -0400

    feat: changed naming for props

--- src/components/AddTaskButton.jsx
+++ src/components/AddTaskButton.jsx
@@ -1,8 +1,8 @@
-export default function Button({ addTaskView, handleAddTask }) {
+export default function Button({ addTaskView, handleToggleTask }) {
   return (
     <button
       className=" bg-primary-500 hover:bg-primary-400 duration-300 text-text-950 w-1/5 p-3 rounded-lg font-semibold"
-      onClick={handleAddTask}
+      onClick={handleToggleTask}
     >
       {addTaskView ? "Close" : "Add Task"}
     </button>
